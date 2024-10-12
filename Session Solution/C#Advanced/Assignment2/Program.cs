1b298af470efb28f3311ee862b049b759df7a639
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2024-10-12 23:57:19 +0100

    Q1 Solution

--- Session Solution/C#Advanced/Assignment2/Program.cs
+++ Session Solution/C#Advanced/Assignment2/Program.cs
@@ -3,28 +3,36 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        PaymentProcess paymentProcess = new PaymentProcess();
-        System.Console.WriteLine("How would like to pay [1]{CreditCardPayment} [2]{BankTransfer} [3]{Paypal}");
-        int choice;
-        while(true)
-            {
-                if(Int32.TryParse(Console.ReadLine(), out choice) && (choice >=1 && choice <=3))
+        #region Q1
+            PaymentProcess paymentProcess = new PaymentProcess();
+            System.Console.WriteLine("How would like to pay [1]{CreditCardPayment} [2]{BankTransfer} [3]{Paypal}");
+            int choice;
+            while(true)
                 {
-                    break;
+                    if(Int32.TryParse(Console.ReadLine(), out choice) && (choice >=1 && choice <=3))
+                    {
+                        break;
+                    }
+                     System.Console.WriteLine("Wrong Selection Enter 1 , 2 or 3");
                 }
-                 System.Console.WriteLine("Wrong Selection Enter 1 , 2 or 3");
+            switch (choice)
+            {
+                case 1:
+                    paymentProcess.Process(new CreditCard(), 100);
+                    break;
+                case 2:
+                    paymentProcess.Process(new BankTransfer(), 100);
+                    break;
+                case 3:
+                    paymentProcess.Process(new PayPal(), 100);
+                    break;
             }
-        switch (choice)
-        {
-            case 1:
-                paymentProcess.Process(new CreditCard(), 100);
-                break;
-            case 2:
-                paymentProcess.Process(new BankTransfer(), 100);
-                break;
-            case 3:
-                paymentProcess.Process(new PayPal(), 100);
-                break;
-        }
+    
+        #endregion    
+        
+        #region Q2
+        
+        #endregion
+    
     }
 }

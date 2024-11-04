583b39d247eff933ea1cb5f96da08cd3f957851a
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2024-11-04 19:11:03 +0000

    BubbleSort using Delegate

---
+++ Session Solution/C#Advanced/Assignment4_Delegate/CompareFunctions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Assignment4_Delegate;
+
+public class CompareFunctions
+{
+    public static bool CompareGreater(int a, int b)
+    {
+        return a > b;
+    }
+
+      public static bool CompareSmaller(int a, int b)
+    {
+        return a < b;
+    }
+    
+}
--- Session Solution/C#Advanced/Assignment4_Delegate/Program.cs
+++ Session Solution/C#Advanced/Assignment4_Delegate/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assignment4_Delegate;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] Numbers = new int[] { 10, 20, 50, 3, 2, 1, 38, 4, 9, 8 };
+        SortingAlgorithm.BubbleSort(Numbers, CompareFunctions.CompareGreater);
+        foreach (var item in Numbers)
+        {
+            Console.WriteLine(item);
+        }
+        System.Console.WriteLine("=====================================");
+        SortingAlgorithm.BubbleSort(Numbers, CompareFunctions.CompareSmaller);
+        foreach (var item in Numbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
